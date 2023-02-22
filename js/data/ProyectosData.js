a024b1a2c94c39edefeec13f3f964e4b655c711e
xDavid-Gabriel <davidbetalleluz31@gmail.com>
2023-02-22 00:29:17 -0500

    Update

---
+++ js/data/ProyectosData.js
@@ -0,0 +1,105 @@
+export const proyectos = [
+  {
+    title: "Mockup Publicitario",
+    img: "img/trabajos/portafolio-1.jpg",
+  },
+  {
+    title: "Mockup Publicitario",
+    img: "img/trabajos/portafolio-2.png",
+  },
+  {
+    title: "Mockup Publicitario",
+    img: "img/trabajos/portafolio-3.jpg",
+  },
+  {
+    title: "Mockup Publicitario",
+    img: "img/trabajos/portafolio-4.jpg",
+  },
+  {
+    title: "Mockup Publicitario",
+    img: "img/trabajos/portafolio-5.png",
+  },
+  {
+    title: "Afiche Publicitario - Banner",
+    img: "img/trabajos/portafolio-6.png",
+  },
+  {
+    title: "Afiche Publicitario - Banner",
+    img: "img/trabajos/portafolio-7.png",
+  },
+
+  {
+    title: "Publicaciones",
+    img: "img/trabajos/portafolio-8.png",
+  },
+  {
+    title: "Post de publicidades",
+    img: "img/trabajos/portafolio-9.png",
+  },
+  {
+    title: "Post de publicidades",
+    img: "img/trabajos/portafolio-10.png",
+  },
+
+  {
+    title: "Publicaciones de Fb",
+    img: "img/trabajos/portafolio-11.png",
+  },
+  {
+    title: "Publicaciones de Fb",
+    img: "img/trabajos/portafolio-12.png",
+  },
+  {
+    title: "Publicaciones de Fb",
+    img: "img/trabajos/portafolio-13.png",
+  },
+  {
+    title: "Logos",
+    img: "img/trabajos/portafolio-14.png",
+  },
+  {
+    title: "Logos",
+    img: "img/trabajos/portafolio-15.png",
+  },
+  {
+    title: "Logos",
+    img: "img/trabajos/portafolio-16.png",
+  },
+  {
+    title: "Logos",
+    img: "img/trabajos/portafolio-17.png",
+  },
+  {
+    title: "Portada de fb",
+    img: "img/trabajos/portafolio-18.png",
+  },
+  {
+    title: "Papeleria corporativa",
+    img: "img/trabajos/portafolio-19.png",
+  },
+  {
+    title: "Papeleria corporativa",
+    img: "img/trabajos/portafolio-20.png",
+  },
+  {
+    title: "Mockups",
+    img: "img/trabajos/portafolio-21.png",
+  },
+  {
+    title: "Papeleria corporativa",
+    img: "img/trabajos/portafolio-22.png",
+  },
+
+  {
+    title: "Papeleria corporativa",
+    img: "img/trabajos/portafolio-26.png",
+  },
+  {
+    title: "Papeleria corporativa",
+    img: "img/trabajos/portafolio-27.png",
+  },
+  {
+    title: "Logo",
+    img: "img/trabajos/portafolio-28.png",
+  },
+];
--- js/main.js
+++ js/main.js
@@ -1,109 +1,118 @@
-
+import { proyectos } from "./data/ProyectosData.js";
 /* =========== Navegacion =========== */
 const list = document.querySelectorAll(".list");
 
-function mueveBotones(){
-  list.forEach((item)=>{
-    item.classList.remove("active")
-  })
-  this.classList.add("active")
-} 
+function mueveBotones() {
+  list.forEach((item) => {
+    item.classList.remove("active");
+  });
+  this.classList.add("active");
+}
 
-list.forEach((item)=>{
-  item.addEventListener("click", mueveBotones)
-  console.log(item);
-})
-
-/*=========== SKILLS TABS ================*/
-
-
-/*=========== MIXITUP FILTER PORTFOLIO ================*/
-let mixerPorfolio = mixitup(".work__container", {
-  selectors: {
-    target: ".card-work",
-  },
-  animation: {
-    duration: 300,
-  },
+list.forEach((item) => {
+  item.addEventListener("click", mueveBotones);
 });
 
-/*=========== Link Active Work ================*/
-const linkWork = document.querySelectorAll(".work__item");
+/*=========== FOTOS DE TRABAJOS  ================*/
+// const workContainer = document.querySelector(".work__container");
+// const loadMoreButton = document.querySelector("#load-more");
+// let numProyectosCargados = 3;
 
-function activeWork() {
-  linkWork.forEach((l) => {
-    l.classList.remove("active-work");
+// const newProyectos = proyectos.slice(0, numProyectosCargados);
+
+// newProyectos.map((proyecto) => {
+//   const div = document.createElement("div");
+//   div.setAttribute("class", "col-12 col-md-6 col-lg-4 card-work");
+//   div.innerHTML =
+//     /*html*/
+//     `
+//     <div class="work__card">
+//     <img
+//       src="${proyecto.img}"
+//       alt="${proyecto.title}"
+//       class="work__img"
+//     />
+//     <h3 class="work__title">${proyecto.title}</h3>
+//     </div>
+
+// `;
+
+//   workContainer.appendChild(div);
+// });
+
+const workCardsContainer = document.querySelector(".work__cards");
+const loadMoreBtn = document.getElementById("load-more-btn");
+const workOverlay = document.querySelector(".work-overlay");
+
+let numProyectosMostrados = 8;
+
+// Función para renderizar las tarjetas de proyectos
+function renderProyectos() {
+  workCardsContainer.innerHTML = "";
+  const proyectosMostrados = proyectos.slice(0, numProyectosMostrados);
+
+  proyectosMostrados.forEach((proyecto, i) => {
+    const card = document.createElement("div");
+    card.className = "work__card";
+    card.innerHTML = `
+    <img src="${proyecto.img}" alt="${proyecto.title}" class="work__img">
+    <h3 class="work__title">${proyecto.title}</h3>
+    `;
+
+    workCardsContainer.appendChild(card);
+
+    const workImg = document.querySelectorAll(".work__img");
+
+    workImg[i].addEventListener("click", () => {
+      workOverlay.innerHTML =
+        /*html*/
+        `<img class="work__img--overlay" src="${workImg[i].getAttribute(
+          "src"
+        )}"  />
+      <button class="work-btn-close"><i class="fa-solid fa-xmark"></i></button>
+      `;
+      workOverlay.classList.add("work-overlay--active");
+
+      //El boton de cerrar
+      const workBtnClose = document.querySelector(".work-btn-close");
+
+      //Para que no se propage
+      const workImgOverlay = document.querySelector(".work__img--overlay");
+
+      workOverlay.addEventListener("click", (event) => {
+        if (event.target.classList.contains("work-btn-close")) {
+          workOverlay.classList.remove("work-overlay--active");
+          workOverlay.innerHTML = "";
+        } else {
+          workOverlay.classList.remove("work-overlay--active");
+          workOverlay.innerHTML = "";
+        }
+      });
+      workImgOverlay.addEventListener("click", (e) => {
+        e.stopPropagation();
+      });
+    });
   });
 
-  this.classList.add("active-work");
-}
-
-linkWork.forEach((l) => {
-  l.addEventListener("click", activeWork);
-});
-
-/*=========== Work Popup ================*/
-
-document.addEventListener("click", (e) => {
-  if (e.target.classList.contains("work__button")) {
-    togglePortfolioPopup();
-    portfolioItemDetails(e.target.parentElement);
+  if (proyectosMostrados.length === proyectos.length) {
+    loadMoreBtn.style.display = "none";
   }
-});
-
-function togglePortfolioPopup() {
-  document.querySelector(".portfolio__popup").classList.toggle("open");
-
-  document
-    .querySelector(".portfolio__popup-inner")
-    .addEventListener("click", (e) => {
-      e.stopPropagation();
-    });
 }
 
-document
-  .querySelector(".portfolio__popup-close")
-  .addEventListener("click", togglePortfolioPopup);
-
-//remover desde el body
-document.querySelector("body").addEventListener("click", () => {
-  //cerrar modales de los portafolios
-  document.querySelector(".portfolio__popup").classList.remove("open");
-});
-
-function portfolioItemDetails(portfolioitem) {
-  document.querySelector(".pp__thumbnail > img").src =
-    portfolioitem.querySelector(".work__img").src;
-  document.querySelector(".portfolio__popup-subtitle > span").innerHTML =
-    portfolioitem.querySelector(".work__title").innerHTML;
-  document.querySelector(".portfolio__popup-body").innerHTML =
-    portfolioitem.querySelector(".portfolio__item-details").innerHTML;
-  /*Por si quiero hacerlo individualmente 1 por 1*/
-  // document.querySelector(".portfolio__popup-body > h3").textContent = portfolioitem.querySelector(".details__title").textContent
-  // document.querySelector(".portfolio__popup-body > p").textContent = portfolioitem.querySelector(".details__description").textContent
-  // document.querySelector(".portfolio__popup-body > ul").innerHTML = portfolioitem.querySelector(".details__info").innerHTML
+// Función que se llama cuando se hace clic en el botón "Ver más"
+function handleLoadMoreClick() {
+  numProyectosMostrados += 3;
+  renderProyectos();
 }
-/*=========== SERVICES MODAL ================*/
 
+// Asignar evento al botón "Ver más"
+loadMoreBtn.addEventListener("click", handleLoadMoreClick);
 
-/*=========== SWIPER TESTIMONIAL ================*/
-let swiper = new Swiper(".testimonial__container", {
-  spaceBetween: 30,
-  loop: true,
-  grabCursor: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 40,
-    },
-  },
-});
+// Renderizar las tarjetas de proyectos iniciales
+renderProyectos();
 
 /*=========== INPUT ANIMATION ================*/
+
 const inputs = document.querySelectorAll(".input");
 
 function focusFunc() {
@@ -126,71 +135,73 @@ inputs.forEach((input) => {
 
 /* ===================== SCROLL SECTIONS ACTIVE LINK =====================*/
 //obtener todas las secciones que tienen una identificación definida
-const sections = document.querySelectorAll("section[id]");
-console.log(sections);
-// agregue un detector de eventos escuchando el desplazamiento
-window.addEventListener("scroll", navHighlighter);
+// const sections = document.querySelectorAll("section[id]");
 
-function navHighlighter() {
-  // obtener la posición de desplazamiento actual
-  let scrollY = window.pageYOffset;
-  // console.log(scrollY);
-  //Ahora recorremos las secciones para obtener los valores de altura, parte superior e ID para cada
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 50,
-    sectionId = current.getAttribute("id")
-    console.log(sectionId);
+// // agregue un detector de eventos escuchando el desplazamiento
+// window.addEventListener("scroll", navHighlighter);
 
-    //(Ingles)
-    /* - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
-    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector */
+// function navHighlighter() {
+//   // obtener la posición de desplazamiento actual
+//   let scrollY = window.pageYOffset;
+//   // console.log(scrollY);
+//   //Ahora recorremos las secciones para obtener los valores de altura, parte superior e ID para cada
+//   sections.forEach((current) => {
+//     const sectionHeight = current.offsetHeight;
+//     const sectionTop = current.offsetTop - 50,
+//       sectionId = current.getAttribute("id");
 
-    //(Español)
-    /*Si nuestra posición de desplazamiento actual ingresa al espacio donde está la sección actual en la pantalla, agregue la clase .active al enlace de navegación correspondiente, de lo contrario, elimínelo
-    - Para saber qué enlace necesita una clase activa, usamos la variable sectionId que obtenemos mientras recorremos las secciones como un selector*/
-    
-   if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-     document.querySelector(".navigation_container li[atributo*=" + sectionId + "]").classList.add("active")
-   }else{
-    document.querySelector(".navigation_container li[atributo*=" + sectionId + "]").classList.remove("active")
-   }
-  });
-}
+//     //(Ingles)
+//     /* - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+//     - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector */
+
+//     //(Español)
+//     /*Si nuestra posición de desplazamiento actual ingresa al espacio donde está la sección actual en la pantalla, agregue la clase .active al enlace de navegación correspondiente, de lo contrario, elimínelo
+//     - Para saber qué enlace necesita una clase activa, usamos la variable sectionId que obtenemos mientras recorremos las secciones como un selector*/
+
+//     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+//       document
+//         .querySelector(".navigation_container li[atributo*=" + sectionId + "]")
+//         .classList.add("active");
+//     } else {
+//       document
+//         .querySelector(".navigation_container li[atributo*=" + sectionId + "]")
+//         .classList.remove("active");
+//     }
+//   });
+// }
 // Que se esconda los temas de colores al hacer scroll
-window.addEventListener("scroll", ()=>{
-  if(document.querySelector(".style-switcher").classList.contains("abrir")){
-      document.querySelector(".style-switcher").classList.remove("abrir")
+window.addEventListener("scroll", () => {
+  if (document.querySelector(".style-switcher").classList.contains("abrir")) {
+    document.querySelector(".style-switcher").classList.remove("abrir");
   }
-})
+});
 
 // Toggle style switcher
-const styleSwitcherToggle = document.querySelector(".style-switcher-toggler")
-styleSwitcherToggle.addEventListener("click",()=>{
-    document.querySelector(".style-switcher").classList.toggle("abrir")
-})
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+  document.querySelector(".style-switcher").classList.toggle("abrir");
+});
 
 // theme colors
-const alternateStyles = document.querySelectorAll(".alternate-style")
+const alternateStyles = document.querySelectorAll(".alternate-style");
 
-const setActiveStyle = (color)=>{
-    
-alternateStyles.forEach((style)=>{
-if(color === style.getAttribute("title")){
-    style.removeAttribute("disabled")
-}else{
-    style.setAttribute("disabled", "true")
-}
-})
-}
+const setActiveStyle = (color) => {
+  alternateStyles.forEach((style) => {
+    if (color === style.getAttribute("title")) {
+      style.removeAttribute("disabled");
+    } else {
+      style.setAttribute("disabled", "true");
+    }
+  });
+};
 
 // theme light and dark mode
 
-const dayNight = document.querySelector(".day-night")
-dayNight.addEventListener("click",()=>{
-  dayNight.querySelector("i").classList.toggle("fa-sun")
-    dayNight.querySelector("i").classList.toggle("fa-moon")
-    // dayNight.querySelector("i").classList.toggle("fa-moon")
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+  dayNight.querySelector("i").classList.toggle("fa-sun");
+  dayNight.querySelector("i").classList.toggle("fa-moon");
+  // dayNight.querySelector("i").classList.toggle("fa-moon")
 
-    document.body.classList.toggle("luz")
-})
+  document.body.classList.toggle("luz");
+});
